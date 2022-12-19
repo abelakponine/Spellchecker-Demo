4dff289d2ceb8634aa22cc097bc1038d2a31dacc
Abel Akponine <abelakponine@gmail.com>
2022-12-19 14:15:41 +0000

    First Commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+    declare(strict_types=1);
+    // Report all errors
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+
+?>
